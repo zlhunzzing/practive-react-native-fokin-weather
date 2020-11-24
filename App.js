bde4e4f804fe622848988ac09472616f9b24b82b
zlhunzzing <zlhunzzing@gmail.com>
2020-11-24 18:10:54 +0900

    API 호출

--- App.js
+++ App.js
@@ -2,20 +2,31 @@ import React from 'react';
 import { Alert } from "react-native"
 import Loading from './Loading'
 import * as Location from 'expo-location'
+import axios from 'axios'
+import { API_KEY } from './env';
 
 export default class extends React.Component {
   state = {
     isLoading: true
   }
 
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data)
+  }
+
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync()
-      const { coords: { latitude, longitude}
-      } = await Location.getCurrentPositionAsync({})
-      this.setState({ isLoading: false })
-      // Send to API
 
+      const { coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync({})
+
+      this.getWeather(latitude, longitude)
+
+      this.setState({ isLoading: false })
     } catch (error) {
       Alert.alert("Can't find you.", "So sad")
     }
